d4f5c9dbb68890ffc40630b40928823a14fe2939
Aleksandr Pervykh <alexander.pervykh.5545@gmail.com>
2024-03-10 00:05:12 +0300

    feat: add solution to makeWord

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -152,8 +152,15 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  const res = [];
+  res.length = Object.values(lettersObject).flat().length;
+  Object.keys(lettersObject).forEach((key) => {
+    lettersObject[key].forEach((elem, index) => {
+      res[lettersObject[key][index]] = key;
+    });
+  });
+  return res.join('');
 }
 
 /**
