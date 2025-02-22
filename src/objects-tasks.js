81a7055efcf0120c827c430c4a652d221318e126
Aleksandr Pervykh <alekspervykh2230521@gmail.com>
2025-02-22 19:35:13 +0300

    feat: add fromJSON

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -260,8 +260,9 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const res = Object.create(proto);
+  return Object.assign(res, JSON.parse(json));
 }
 
 /**
